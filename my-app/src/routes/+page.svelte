602f0e381814e632c49546da4c9f4c1af475a7eb
TheQuate <123550040+TheQuate@users.noreply.github.com>
2024-03-06 09:55:21 +0100

    Update +page.svelte

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -8,14 +8,20 @@
 
 
 
-
 <center class="background">
-    <body>
-        <h3 class="ovn">You found the easter egg</h3>
-        <button class="knappDrit" on:click={increaseCounter}> Du har klikket {counter} ganger</button>
+    <div class="box">
+        Settings
+    </div>
+    <button class="knappDrit" on:click={increaseCounter}> Du har klikket {counter} ganger</button>
+    <center>
+    
+        <body>
+        <h3 class="ovn">You found the easter egg</h3> 
     </body>
    
 </center>
+</center>
+
 
 <style>
     .knappDrit{
@@ -26,27 +32,33 @@
         display: inline-block;
         font-size: 24px;
         padding: 15px 32px;
-        margin: 200px;
-        
+        margin-top: 10px;
         
     }
 
     .ovn{
-        color: black;
+        color: darkgrey;
         background-color: darkgray;
         padding: 50px;
         border: none;
         font-size: 23px;
-        width: 1200px;
-        margin: 200px;
+        width: 1500px;
+        margin: 0px;
+        height: 600px;
     }
 
     .background{
         background-color: grey;
         border: none;
-        
+        height: 1000px;
 
     }
-
-    
+    .box{
+        background-color: rgb(75, 73, 72);
+        width: 1810px;
+        height: 100px;
+        border: 3px solid black;
+        padding: 10px;
+        
+    }
 </style>
